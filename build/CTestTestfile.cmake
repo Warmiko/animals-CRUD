8a171b01d609efae916463c9ac8ed75132c34fa4
Warmiko <mikowarc@gmail.com>
2023-08-24 11:37:40 +0200

    Added gtest

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/Programming/C++/Cpp_projects/animals-CRUD
+# Build directory: E:/Programming/C++/Cpp_projects/animals-CRUD/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("E:/Programming/C++/Cpp_projects/animals-CRUD/build/Animals-CRUD-ut[1]_include.cmake")
+subdirs("_deps/googletest-build")
---
+++ wymagania.md
@@ -0,0 +1,9 @@
+CRUD Project functionality:
+	-storing records with attributes 'Imie' 'Nazwisko' 'adres' 'PESEL' 'plec'
+	-displaying all records
+	-adding a new record
+	-searching by name
+	-searching by id
+	-deleting records by id
+	-sorting by id
+	-sorting by name
